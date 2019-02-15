8456bc4d37f2abf06401c0f5bcd594814eb8dfbf
Lightman <greenmailse@mail.ru>
2019-02-15 16:02:50 +0400

    task part2

--- app/src/main/java/com/company/aircraft/todo/MainActivity.java
+++ app/src/main/java/com/company/aircraft/todo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.company.aircraft.todo;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,24 +10,43 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText edittext;
-    Button button;
+    Button btnadd;
     TextView txview;
-
+    int i = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onMyButtonClick() {
+        btnadd = (Button) findViewById(R.id.button2);
         edittext = (EditText) findViewById(R.id.plain_text_input);
         txview = (TextView) findViewById(R.id.textView);
-        String textvalue = edittext.getText().toString();
-        if (textvalue != null) {
 
-        }
+
+        View.OnClickListener oclbtnadd = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<TextView> txt = new ArrayList<>();
+                for (int i=0; i<10; i++) {
+                    TextView txtItem = new TextView(getApplicationContext());
+                    txt.add(txtItem);
+                    ConstraintLayout.(txtItem);
+
+                String textvalue = edittext.getText().toString();
+                        if (textvalue != null) {
+                            txview.setText(textvalue);
+                        } else {
+                            txview.setText(null);
+                        }
+
+                    }
+            }
+        };
+        btnadd.setOnClickListener(oclbtnadd);
     }
 }
